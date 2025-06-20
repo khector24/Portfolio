82efdbe0ba02251474aa0b6cf21afc702c4a3dce
Kenny Hector <kennyhector24@gmail.com>
2025-06-19 21:20:17 -0400

    Remove testimonials link and rename classes.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,60 +4,59 @@ import "../styles/component-styles/Footer.css";
 export default function Footer() {
   return (
     <footer className="footer">
-      <div className="footer-top-content">
-        <h2>Let's Connect!</h2>
-        <p>
-          I'm always open to discussing new projects, creative ideas, or
-          opportunities to be part of your vision. Feel free to reach out!
-        </p>
-        <a
-          href="mailto:kennyhector24@gmail.com"
-          className="footer-contact-button"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Contact Me
-        </a>
-      </div>
-      <div className="middle-content">
-        <Link to="/" className="nav-link">
-          Home
-        </Link>
-        <Link to="/portfolio" className="nav-link">
-          Projects
-        </Link>
-        <Link to="/testimonials" className="nav-link">
-          Testimonials
-        </Link>
-        <Link to="/about" className="nav-link">
-          About
-        </Link>
-        <Link to="/contact" className="nav-link">
-          Contact
-        </Link>
+      <div className="footer-top">
+        <div className="footer-contact">
+          <h2>Let's Connect!</h2>
+          <p>
+            I'm always open to discussing new projects, creative ideas, or
+            opportunities to be part of your vision. Feel free to reach out!
+          </p>
+          <a
+            href="mailto:kennyhector24@gmail.com"
+            className="footer-contact-button"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Contact Me
+          </a>
+          <p>
+            Follow me on{" "}
+            <a
+              href="https://github.com/khector24"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub
+            </a>{" "}
+            |{" "}
+            <a
+              href="https://www.linkedin.com/in/kennyhector"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              LinkedIn
+            </a>
+          </p>
+        </div>
+        <div className="footer-nav">
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+          <Link to="/portfolio" className="nav-link">
+            Projects
+          </Link>
+          <Link to="/about" className="nav-link">
+            About
+          </Link>
+          <Link to="/contact" className="nav-link">
+            Contact
+          </Link>
+        </div>
       </div>
       <hr />
-      <div className="bottom-content">
+      <div className="footer-bottom">
         <p>
           &copy; {new Date().getFullYear()} Kenny Hector. All rights reserved.
-        </p>
-        <p>
-          Follow me on{" "}
-          <a
-            href="https://github.com/khector24"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            GitHub
-          </a>{" "}
-          |{" "}
-          <a
-            href="https://www.linkedin.com/in/kennyhector"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            LinkedIn
-          </a>
         </p>
         <p>
           Icons by{" "}
