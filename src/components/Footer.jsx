b29404549b7306c6df5b71c54d12d0e63a753a49
Kenny Hector <kennyhector24@gmail.com>
2025-06-19 21:03:24 -0400

    Add extra content to foooter and make it take the full screen height.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,43 @@
+import { Link } from "react-router-dom";
 import "../styles/component-styles/Footer.css";
 
 export default function Footer() {
   return (
     <footer className="footer">
-      <div className="footer-content">
+      <div className="footer-top-content">
+        <h2>Let's Connect!</h2>
+        <p>
+          I'm always open to discussing new projects, creative ideas, or
+          opportunities to be part of your vision. Feel free to reach out!
+        </p>
+        <a
+          href="mailto:kennyhector24@gmail.com"
+          className="footer-contact-button"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Contact Me
+        </a>
+      </div>
+      <div className="middle-content">
+        <Link to="/" className="nav-link">
+          Home
+        </Link>
+        <Link to="/portfolio" className="nav-link">
+          Projects
+        </Link>
+        <Link to="/testimonials" className="nav-link">
+          Testimonials
+        </Link>
+        <Link to="/about" className="nav-link">
+          About
+        </Link>
+        <Link to="/contact" className="nav-link">
+          Contact
+        </Link>
+      </div>
+      <hr />
+      <div className="bottom-content">
         <p>
           &copy; {new Date().getFullYear()} Kenny Hector. All rights reserved.
         </p>
