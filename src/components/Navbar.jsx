5a7c1ba3ffbce37a2f88a5d443112ed4d9d6c3a9
Kenny Hector <kennyhector24@gmail.com>
2025-06-25 21:23:27 -0400

    Rework navbar to have a menu-icon that upon clicking will show nav links.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,42 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/component-styles/Navbar.css";
+import MenuBar from "../assets/icons/menu-bar.svg";
 
 export default function Navbar() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <nav>
       <div className="nav-left">
-        <span>Kenny Hector</span>
+        <span>Kenny H.</span>
       </div>
-      <div className="nav-right">
-        <Link to="/" className="nav-link">
-          Home
-        </Link>
-        <Link to="/about" className="nav-link">
-          About
-        </Link>
-        <Link to="/portfolio" className="nav-link">
-          Portfolio
-        </Link>
-        <Link to="/contact" className="nav-link">
-          Contact
-        </Link>
-      </div>
+      <img
+        src={MenuBar}
+        alt="Menu Bar"
+        className="menu-bar"
+        onClick={toggleMenu}
+      />
+      {showMenu && (
+        <div className="nav-right">
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+          <Link to="/about" className="nav-link">
+            About
+          </Link>
+          <Link to="/portfolio" className="nav-link">
+            Portfolio
+          </Link>
+          <Link to="/contact" className="nav-link">
+            Contact
+          </Link>
+        </div>
+      )}
     </nav>
   );
 }
