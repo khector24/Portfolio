f8440b53c5845f102fa7433ffa034fa0f227f564
Kenny Hector <kennyhector24@gmail.com>
2025-06-25 21:59:50 -0400

    Add icons to href.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/component-styles/Navbar.css";
 import MenuBar from "../assets/icons/menu-bar.svg";
+import ResumeIcon from "../assets/icons/resume-icon.svg";
+import EmailIcon from "../assets/icons/email-icon.svg";
+import GitHubIcon from "../assets/icons/github-icon.svg";
 
 export default function Navbar() {
   const [showMenu, setShowMenu] = useState(false);
@@ -39,9 +42,18 @@ export default function Navbar() {
           </Link>
           <hr />
           <div>
-            <a href="mailto:kennyhector24@gmail.com">kennyhector24@gmail.com</a>
+            <a href="mailto:kennyhector24@gmail.com">
+              <img src={<EmailIcon />} alt="" />
+            </a>
             <a href="" className="nav-link">
-              Resume
+              <img src={<ResumeIcon />} alt="" />
+            </a>
+            <a
+              href="https://github.com/khector24"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={<GitHubIcon />} alt="" />
             </a>
           </div>
         </div>
