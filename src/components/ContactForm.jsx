18299f638d0e933b1b45d10acf2886e04d141996
Kenny Hector <kennyhector24@gmail.com>
2025-06-26 19:00:09 -0400

    Create contact page and form.

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import "../styles/component-styles/ContactForm.css";
+
+export default function ContactForm() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (evt) => {
+    setFormData((currData) => {
+      return {
+        ...currData,
+        [evt.target.name]: evt.target.value,
+      };
+    });
+  };
+
+  return (
+    <form className="contact-form">
+      <label htmlFor="name">Name:</label>
+      <input
+        type="text"
+        placeholder="Your name"
+        name="name"
+        id="name"
+        value={formData.name}
+        onChange={handleChange}
+      />
+
+      <label htmlFor="email">Email:</label>
+      <input
+        type="email"
+        placeholder="Your email"
+        name="email"
+        id="email"
+        value={formData.email}
+        onChange={handleChange}
+      />
+
+      <label htmlFor="message">Message:</label>
+      <input
+        type="text"
+        placeholder="Your message"
+        name="message"
+        id="message"
+        value={formData.message}
+        onChange={handleChange}
+      />
+    </form>
+  );
+}
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,24 +1,17 @@
+import "../styles/page-styles/Contact.css";
+import ContactForm from "../components/ContactForm";
+
 export default function Contact() {
   return (
-    <div className="contact">
-      <h1>Contact Me</h1>
+    <div className="contact section-full-height bg-light">
+      <h2>Contact Me</h2>
       <p>
         If you have any questions or would like to get in touch, feel free to
         reach out via the contact form below. I look forward to hearing from
         you!
       </p>
-      <form>
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" name="name" required />
 
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" name="email" required />
-
-        <label htmlFor="message">Message:</label>
-        <textarea id="message" name="message" required></textarea>
-
-        <button type="submit">Send Message</button>
-      </form>
+      <ContactForm />
     </div>
   );
 }
