cf44eae4af51894bacf734d7d6fc71efc019d5a1
Kenny Hector <kennyhector24@gmail.com>
2025-06-26 22:49:35 -0400

    Style updates.

--- src/pages/Career.jsx
+++ src/pages/Career.jsx
@@ -1,12 +1,10 @@
-// src/pages/Career.jsx
-
 import "../styles/page-styles/Career.css";
 import { resumeData } from "../assets/data";
 import CareerCard from "../components/CareerCard";
 
 export default function Career() {
   return (
-    <div className="career-container section-full-height">
+    <div className="career-container">
       <h1>Career & Achievements</h1>
       <div className="career-list">
         {resumeData.map((job, index) => (
