7846080aaeb4aa2b732446f7e553e143cef24878
Kenny Hector <kennyhector24@gmail.com>
2025-06-28 00:32:16 -0400

    Add carousel to images to be able to scroll.

--- src/components/PortfolioCard.jsx
+++ src/components/PortfolioCard.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "../styles/component-styles/PortfolioCard.css";
 
 export default function PortfolioCard({
@@ -8,13 +9,35 @@ export default function PortfolioCard({
   tags,
   images,
 }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handlePrev = () => {
+    setCurrentIndex((prev) => (prev === 0 ? images.length - 1 : prev - 1));
+  };
+
+  const handleNext = () => {
+    setCurrentIndex((prev) => (prev === images.length - 1 ? 0 : prev + 1));
+  };
+
+  const currentImg =
+    images?.[currentIndex] || "/assets/portfolio-pics/black.png";
+
   return (
     <div className="portfolio-card">
-      <img
-        src={images?.[0] || "/assets/portfolio-pics/black.png"}
-        alt={title}
-        className="project-img"
-      />
+      <div className="carousel-wrapper">
+        <button className="arrow left" onClick={handlePrev}>
+          &#8249;
+        </button>
+        <img
+          src={currentImg}
+          alt={`${title}-${currentIndex}`}
+          className="project-img"
+        />
+        <button className="arrow right" onClick={handleNext}>
+          &#8250;
+        </button>
+      </div>
+
       <h3>{title}</h3>
       <p className="description">{description}</p>
       <p className="tech">{tech}</p>
