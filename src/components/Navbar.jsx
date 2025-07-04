951f38918859151249ff6ea72c77e31b65e356a6
Kenny Hector <kennyhector24@gmail.com>
2025-07-03 23:13:56 -0400

    Add useEffect and useRef to make navbar closeable by cliking anywhere on the website.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "../styles/component-styles/Navbar.css";
 import MenuBar from "../assets/icons/menu-bar.svg";
@@ -9,6 +9,23 @@ import resumePDF from "../assets/documents/resume.pdf";
 
 export default function Navbar() {
   const [showMenu, setShowMenu] = useState(false);
+  const menuRef = useRef();
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setShowMenu(false);
+      }
+    }
+
+    if (showMenu) {
+      document.addEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [showMenu]);
 
   const toggleMenu = () => {
     setShowMenu(!showMenu);
@@ -20,50 +37,47 @@ export default function Navbar() {
         <span>Kenny H.</span>
       </div>
 
-      <img
-        src={MenuBar}
-        alt="Menu Bar"
-        className={`menu-bar ${showMenu ? "" : "rotate"}`}
-        onClick={toggleMenu}
-      />
+      <div className="menu-container" ref={menuRef}>
+        <img
+          src={MenuBar}
+          alt="Menu Bar"
+          className={`menu-bar ${showMenu ? "" : "rotate"}`}
+          onClick={toggleMenu}
+        />
 
-      {showMenu && (
-        <div className="nav-right">
-          <Link to="/" className="nav-link">
-            Home
-          </Link>
-          {/* <Link to="/about" className="nav-link">
-            About
-          </Link> */}
-          <Link to="/career" className="nav-link">
-            Career
-          </Link>
-          <Link to="/portfolio" className="nav-link">
-            Portfolio
-          </Link>
-          <Link to="/contact" className="nav-link">
-            Contact
-          </Link>
-
-          <hr />
-
-          <div className="nav-icons">
-            <a href="mailto:kennyhector24@gmail.com">
-              <img src={EmailIcon} alt="Email" />
-            </a>
-            <a href={resumePDF} target="_blank" rel="noreferrer">
-              <img src={ResumeIcon} alt="Resume" />
-            </a>
-            <a
-              href="https://github.com/khector24"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img src={GitHubIcon} alt="GitHub" />
-            </a>
+        {showMenu && (
+          <div className="nav-right">
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
+            <Link to="/career" className="nav-link">
+              Career
+            </Link>
+            <Link to="/portfolio" className="nav-link">
+              Portfolio
+            </Link>
+            <Link to="/contact" className="nav-link">
+              Contact
+            </Link>
+            <hr />
+            <div className="nav-icons">
+              <a href="mailto:kennyhector24@gmail.com">
+                <img src={EmailIcon} alt="Email" />
+              </a>
+              <a href={resumePDF} target="_blank" rel="noreferrer">
+                <img src={ResumeIcon} alt="Resume" />
+              </a>
+              <a
+                href="https://github.com/khector24"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={GitHubIcon} alt="GitHub" />
+              </a>
+            </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
     </nav>
   );
 }
