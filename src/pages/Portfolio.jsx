9d5d72c62a90a5c4b42544b8a4eb5fa7453dfb32
Kenny Hector <kennyhector24@gmail.com>
2025-07-08 16:28:10 -0400

    Update portfolio page styles and add media queries.

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -6,7 +6,9 @@ export default function Portfolio() {
   return (
     <div className="portfolio">
       <h1>My Portfolio</h1>
-      <p>Here are a few of my most impactful full-stack projects:</p>
+      <p className="sub-heading">
+        Here are a few of my most impactful full-stack projects:
+      </p>
       <div className="projects">
         {featuredProjects.map((proj, i) => (
           <PortfolioCard key={i} {...proj} />
